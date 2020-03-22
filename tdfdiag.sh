34a621ee19abb178a4b110c3ae218a22f658537f
Kevin Havens W9JYD <58867813+W9JYD@users.noreply.github.com>
2020-03-22 19:51:02 -0400

    Update tdfdiag.sh
    Changed Browser to default installed on user's system via `x-www-browser` because some use Firefox instead of Chrome

--- tdfdiag.sh
+++ tdfdiag.sh
@@ -5,5 +5,5 @@
 # Some Rights Reserved <https://creativecommons.org/licenses/by-sa/4.0/>
 
 yad  --no-escape --text-align=left --fixed --skip-taskbar --image="dialog-error-symbolic" --title="Site problem!" --text="https://dollforum.com/forum/index.php is a FUCKING RETARDED SITE!\nPress OK to visit Dollsbook.com, a better one!" --button=gtk-ok:0
-google-chrome-stable --new-window https://dollsbook.com > /dev/null 2>&1
+x-www-browser https://dollsbook.com > /dev/null 2>&1
 
