9ceda0b88a6204642e6fe420a6c60b5143d491f2
Kevin Havens W9JYD <58867813+W9JYD@users.noreply.github.com>
2020-03-22 19:51:51 -0400

    Update tdfdiag.sh
    Edited info in comments

--- tdfdiag.sh
+++ tdfdiag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # TDF is Retarded. A joke URL shortcut that actually works. Use tdf.desktop to launch.
-# CC BY-SA 4.0 2019-03-21 Alexandra Tilbrook KI5JYD <ki5jyd@arrl.net> <https://dollsbook.com/members/ki5jyd>
+# CC BY-SA 4.0 2020-03-22 Kevin Havens W9JYD <https://dollsbook.com/members/ki5jyd>
 # Some Rights Reserved <https://creativecommons.org/licenses/by-sa/4.0/>
 
 yad  --no-escape --text-align=left --fixed --skip-taskbar --image="dialog-error-symbolic" --title="Site problem!" --text="https://dollforum.com/forum/index.php is a FUCKING RETARDED SITE!\nPress OK to visit Dollsbook.com, a better one!" --button=gtk-ok:0
